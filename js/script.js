210b07b2f2ec0ef1de7bf08e950e65d4f1327c4a
AngeloTripodi <tripodi.angelo@gmail.com>
2022-11-18 16:53:46 +0100

    Setup Project

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let nome = prompt("Ciao, Inserisci il tuo Nome")
+let cognome = prompt("Inserisci il tuo Cognome")
+let colorePreferito = prompt("Adesso inserisci il tuo colore preferito")
+
+console.log(nome + cognome + colorePreferito + 22);
